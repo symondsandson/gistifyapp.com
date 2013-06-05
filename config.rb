29feb8f9d16014c1c6176a24f8fa0ca81054cba7
Josh Symonds <veraticus@gmail.com>
2013-06-05 11:44:00 -0500

    Improving build process

--- config.rb
+++ config.rb
@@ -56,21 +56,23 @@ set :images_dir, 'img'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
+
+  activate :directory_indexes
 
   # Use relative URLs
   # activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+  require "middleman-smusher"
+  activate :smusher
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
